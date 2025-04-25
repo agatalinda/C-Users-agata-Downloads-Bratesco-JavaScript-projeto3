4ecde09f418e1b104d2661e2caeaceec4eb5cf68
ana caroline <agatalinda2027@gmail.com>
2025-04-25 06:17:45 -0400

    adicionei o ligar e desligar no arquivo úsuario

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -3,6 +3,16 @@ public class SmartTv {
     boolean ligado=false;
     int canal=1;
     int volume=25;
+
+    // criar o método de manipulação do ligar
+
+    public void ligar(){
+        ligado=true;
+       
+    }
+    public void desligado(){
+        ligado=false;
     
+    }
     
 }
--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -1,8 +1,14 @@
 public class Usuario {
     public static void main(String[] args) throws Exception {
        SmartTv smartTv = new SmartTv();
-       System.out.println("Tv ligada ?" + smartTv.ligado);
-       System.out.println("Canal Atual ?" + smartTv.canal);
-       System.out.println("Volume Atual ?" + smartTv.volume);
+       System.out.println("Tv ligada ? " + smartTv.ligado);// Foi adicionado os comando para o usuario ter acesso a Televisão.
+       System.out.println("Canal Atual ? " + smartTv.canal);
+       System.out.println("Volume Atual ? " + smartTv.volume);
+    
+       smartTv.ligar();
+       System.out.println("Novo Smart -> Tv ligada ? " + smartTv.ligado);
+    
+       smartTv.desligado();
+       System.out.println("Novo Smart -> Tv desligada ? " + smartTv.ligado);
     }
 }
