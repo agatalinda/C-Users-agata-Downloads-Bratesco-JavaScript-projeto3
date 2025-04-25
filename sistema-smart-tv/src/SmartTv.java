f5b6eb73af6a980d01d66195a6213590377c8a0a
ana caroline <agatalinda2027@gmail.com>
2025-04-25 06:26:19 -0400

    adicionado o comando aumentar e diminiuir volume.

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -1,8 +1,19 @@
-public class SmartTv {
+public class SmartTv { // Aqui é a configurações da tv
 
     boolean ligado=false;
     int canal=1;
     int volume=25;
+    public void aumentarVolume(){ // Aqui é o método para aumentar o volume
+        //volume = volume + 1; // Aqui é o método para aumentar o volume
+        //volume++; // Aqui é o método para aumentar o volume
+        volume++;
+    }
+    public void diminuirVolume(){ // Aqui é o método para diminuir o volume
+        //volume = volume - 1; // Aqui é o método para diminuir o volume
+        //volume--; // Aqui é o método para diminuir o volume
+        volume--;
+    }
+
 
     // criar o método de manipulação do ligar
 
