a9428fd140183856b74082752c211781b04ba876
ana caroline <agatalinda2027@gmail.com>
2025-04-28 16:23:32 -0400

    Eu apliquei  o comando API para olhar a classe na web

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -3,6 +3,21 @@ public class SmartTv { // Aqui é a configurações da tv
     boolean ligado=false;
     int canal=1;
     int volume=25;
+    public void mudarCnal(int novoCanal){ // Aqui é o método para mudar o canal
+        canal=novoCanal; // Aqui é o método para mudar o canal
+        System.out.println("Mudando para o canal: " + canal); // Aqui é o método para mudar o canal
+    }
+
+    public void aumentarCanal(){
+          // Canal para ajutar o usuario a entender o funcionamento da tv
+        canal++;
+
+
+    };
+    public void diminuirCanal(){
+        canal--;
+    };
+
     public void aumentarVolume(){ // Aqui é o método para aumentar o volume
         //volume = volume + 1; // Aqui é o método para aumentar o volume
         //volume++; // Aqui é o método para aumentar o volume
--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -4,12 +4,18 @@ public class Usuario {
 
        SmartTv smartTv = new SmartTv();
 
+
        smartTv.diminuirVolume(); // Aqui é o método para aumentar o volume
+       smartTv.diminuirVolume();// cada comando diminui diminuirá o volume
        smartTv.diminuirVolume();
-       smartTv.diminuirVolume();
+       smartTv.aumentarVolume();// cada comando aumentará o volume
+       smartTv.aumentarVolume();
        smartTv.aumentarVolume();
        System.out.println("Volume Atual ? " + smartTv.volume);
-       
+
+       smartTv.mudarCnal(13); // Aqui é o método para mudar o canal 
+       System.out.println("Canal Atual ? " + smartTv.canal);
+
      // Aqui é o método para aumentar o volume
        System.out.println("Tv ligada ? " + smartTv.ligado);// Foi adicionado os comando para o usuario ter acesso a Televisão.
        System.out.println("Canal Atual ? " + smartTv.canal);
